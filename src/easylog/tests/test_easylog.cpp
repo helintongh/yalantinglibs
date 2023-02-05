93088569c67c507901760a6f2b95fdfd75fedfd2
helintongh <agh6399@qq.com>
2023-02-05 16:39:45 +0800

    feat: add unittest for easylog

--- src/easylog/tests/test_easylog.cpp
+++ src/easylog/tests/test_easylog.cpp
@@ -62,3 +62,60 @@ TEST_CASE("test basic") {
   ELOGD << "debug log";
   CHECK(get_last_line(filename).rfind("debug log") != std::string::npos);
 }
+
+TEST_CASE("test_severity") {
+  std::string filename = "test_severity.txt";
+  std::filesystem::remove(filename);
+  easylog::init_log(Severity::WARN, filename, true, 5000, 1, true);
+
+  ELOGV(ERROR, "error log can put in file.") // 26bytes
+  ELOGV(INFO, "info log can't put in file.")
+  CHECK(get_last_line(filename).rfind("info log can't put in file.") == std::string::npos);
+  CHECK(get_last_line(filename).rfind("info log can't put in file.") != std::string::npos);
+  CHECK(std::filesystem::file_size(filename) == 26);
+
+  ELOG(INFO) << "test log";
+  // init_log four parameter is true,flush every time
+  CHECK(get_last_line(filename).rfind("test log") != std::string::npos);
+  ELOG(WARN) << "test warn log";
+  esaylog::flush();
+  CHECK(get_last_line(filename).rfind("test warn log") != std::string::npos);
+}
+
+TEST_CASE("test_flush") {
+  std::string filename = "test_flush.txt";
+  std::filesystem::remove(filename);
+  easylog::init_log(Severity::DEBUG, filename, true, 5000, 1, false);
+
+  ELOG(INFO) << "test log";
+  CHECK(get_last_line(filename).rfind("test log") == std::string::npos);
+  esaylog::flush();
+  CHECK(get_last_line(filename).rfind("test log") != std::string::npos);
+
+  filename = "test_flush_true_file.txt";
+  std::filesystem::remove(filename);
+  easylog::init_log(Severity::DEBUG, filename, true, 5000, 1, true);
+  ELOG(INFO) << "test log";
+  CHECK(get_last_line(filename).rfind("test log") != std::string::npos);
+}
+
+#if defined(linux) || defined(__linux) || defined(__linux__)
+TEST_CASE("test_terminal_output") {
+  std::string filename = "test_terminal_output.txt";
+  std::filesystem::remove(filename);
+  easylog::init_log(Severity::DEBUG, filename, true, 5000, 1, true);
+  std::stringstream buffer;
+  std::streambuf * old = std::cout.rdbuf(buffer.rdbuf());
+  ELOG(INFO) << "test log";
+  std::string text = buffer.str();
+  CHECK(text.rfind("test log") != std::string::npos);
+
+  filename = "test_terminal_no_output.txt";
+  std::filesystem::remove(filename);
+  easylog::init_log(Severity::DEBUG, filename, false, 5000, 1, true);
+  ELOG(INFO) << "test no log";
+  std::string text = buffer.str();
+  CHECK(text.rfind("test no log") == std::string::npos);
+}
+#endif
+
